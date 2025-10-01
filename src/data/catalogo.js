f007d3f85d45aa03eccee4a39baea7d94532170f
TransmetalAG <transmetalag@gmail.com>
2025-10-01 16:09:20 -0600

    Update catalogo.js

--- src/data/catalogo.js
+++ src/data/catalogo.js
@@ -1,5 +1,121 @@
+// src/data/catalogo.js
+
 export const catalogo = [
-  { maquina: "Troqueladora 1", proceso: "Carretas", eph: 10 },
-  { maquina: "Troqueladora 2", proceso: "Canales", eph: 12 },
-  { maquina: "Lamina Plástica", proceso: "Lámina", eph: 8 }
-]
+  {
+    maquina: "Troqueladora 1",
+    procesos: [
+      { nombre: "Perforado de chumacera", eph: 6.667 },
+    ],
+  },
+  {
+    maquina: "Troqueladora 2",
+    procesos: [
+      { nombre: "Regidez de patilla", eph: 6.667 },
+    ],
+  },
+  {
+    maquina: "Troqueladora 3",
+    procesos: [
+      { nombre: "Perforado de hembra", eph: 3.722 },
+    ],
+  },
+  {
+    maquina: "Troqueladora 5",
+    procesos: [
+      { nombre: "Embutido de chumacera", eph: 11.167 },
+    ],
+  },
+  {
+    maquina: "Troqueladora 6",
+    procesos: [
+      { nombre: "Formado en pata en V", eph: 6.667 },
+    ],
+  },
+  {
+    maquina: "Tubo",
+    procesos: [
+      { nombre: "Tubo", eph: 2.8 },
+    ],
+  },
+  {
+    maquina: "Cab. Soldadura 1",
+    procesos: [
+      { nombre: "Capirucho", eph: 3.333 },
+    ],
+  },
+  {
+    maquina: "Cab. Soldadura 2",
+    procesos: [
+      { nombre: "Base", eph: 2.667 },
+    ],
+  },
+  {
+    maquina: "Sierra de Cinta",
+    procesos: [
+      { nombre: "Tubito", eph: 0 },
+    ],
+  },
+  {
+    maquina: "Caimán",
+    procesos: [
+      { nombre: "Caimán", eph: 2.917 },
+    ],
+  },
+  {
+    maquina: "Troqueladora de plato",
+    procesos: [
+      { nombre: "Plato", eph: 7.867 },
+    ],
+  },
+  {
+    maquina: "Embutidora 315",
+    procesos: [
+      { nombre: "-", eph: 0 },
+    ],
+  },
+  {
+    maquina: "Lavado de tolva",
+    procesos: [
+      { nombre: "Lavado de tolva", eph: 1.0 },
+    ],
+  },
+  {
+    maquina: "Pintura Naranja",
+    procesos: [
+      { nombre: "-", eph: 0 },
+    ],
+  },
+  {
+    maquina: "Pintura Negra",
+    procesos: [
+      { nombre: "-", eph: 0 },
+    ],
+  },
+  {
+    maquina: "Serigrafía",
+    procesos: [
+      { nombre: "Serigrafía", eph: 3.722 },
+    ],
+  },
+  {
+    maquina: "Ensamble de carreta",
+    procesos: [
+      { nombre: "Ensamble de carreta", eph: 1.0 },
+    ],
+  },
+  {
+    maquina: "Apretado de carreta",
+    procesos: [
+      { nombre: "Apretado de carreta", eph: 1.667 },
+    ],
+  },
+  {
+    maquina: "Troqueladora 7",
+    procesos: [
+      { nombre: "Canal liviana", eph: 7.333 },
+      { nombre: "Canal 4.5", eph: 6.2 },
+      { nombre: "Formado en pata en V liviana", eph: 14.0 },
+      { nombre: "Formado en pata en V 4.5", eph: 14.0 },
+    ],
+  },
+];
