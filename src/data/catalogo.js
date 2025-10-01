2ca5e2623df5cd4c048f6208f88c51ae75086aa8
TransmetalAG <transmetalag@gmail.com>
2025-10-01 16:53:11 -0600

    Update catalogo.js

--- src/data/catalogo.js
+++ src/data/catalogo.js
@@ -1,120 +1,24 @@
-// src/data/catalogo.js
 export const catalogo = [
-  {
-    maquina: "Troqueladora 1",
-    procesos: [
-      { nombre: "Perforado de chumacera", eph: 6.667 }
-    ]
-  },
-  {
-    maquina: "Troqueladora 2",
-    procesos: [
-      { nombre: "Regidez de patilla", eph: 6.667 }
-    ]
-  },
-  {
-    maquina: "Troqueladora 3",
-    procesos: [
-      { nombre: "Perforado de hembra", eph: 3.722 }
-    ]
-  },
-  {
-    maquina: "Troqueladora 5",
-    procesos: [
-      { nombre: "Embutido de chumacera", eph: 11.167 }
-    ]
-  },
-  {
-    maquina: "Troqueladora 6",
-    procesos: [
-      { nombre: "Formado en pata en V", eph: 6.667 }
-    ]
-  },
-  {
-    maquina: "Tubo",
-    procesos: [
-      { nombre: "Tubo", eph: 2.800 }
-    ]
-  },
-  {
-    maquina: "Cab. Soldadura 1",
-    procesos: [
-      { nombre: "Capirucho", eph: 3.333 }
-    ]
-  },
-  {
-    maquina: "Cab. Soldadura 2",
-    procesos: [
-      { nombre: "Rueda", eph: 2.667 }
-    ]
-  },
-  {
-    maquina: "Sierra de Cinta",
-    procesos: [
-      { nombre: "Tubito", eph: 0 }
-    ]
-  },
-  {
-    maquina: "Caimán",
-    procesos: [
-      { nombre: "Caimán", eph: 2.917 }
-    ]
-  },
-  {
-    maquina: "Troqueladora de plato",
-    procesos: [
-      { nombre: "Plato", eph: 7.867 }
-    ]
-  },
-  {
-    maquina: "Embutidora 315",
-    procesos: [
-      { nombre: "-", eph: 0 }
-    ]
-  },
-  {
-    maquina: "Lavado de tolva",
-    procesos: [
-      { nombre: "Lavado de tolva", eph: 1.000 }
-    ]
-  },
-  {
-    maquina: "Pintura Naranja",
-    procesos: [
-      { nombre: "-", eph: 0 }
-    ]
-  },
-  {
-    maquina: "Pintura Negra",
-    procesos: [
-      { nombre: "-", eph: 0 }
-    ]
-  },
-  {
-    maquina: "Serigrafía",
-    procesos: [
-      { nombre: "Serigrafía", eph: 3.722 }
-    ]
-  },
-  {
-    maquina: "Ensamble de carreta",
-    procesos: [
-      { nombre: "Ensamble de carreta", eph: 1.000 }
-    ]
-  },
-  {
-    maquina: "Apretado de carreta",
-    procesos: [
-      { nombre: "Apretado de carreta", eph: 1.667 }
-    ]
-  },
-  {
-    maquina: "Troqueladora 7",
-    procesos: [
-      { nombre: "Canal liviana", eph: 7.333 },
-      { nombre: "Canal 4.5", eph: 6.200 },
-      { nombre: "Formado en pata en V liviana", eph: 14.000 },
-      { nombre: "Formado en pata en V 4.5", eph: 14.000 }
-    ]
-  }
+  { maquina: "Troqueladora 1", proceso: "Perforado de chumacera", eph: 6.667 },
+  { maquina: "Troqueladora 2", proceso: "Regidez de patilla", eph: 6.667 },
+  { maquina: "Troqueladora 3", proceso: "Perforado de hembra", eph: 3.722 },
+  { maquina: "Troqueladora 5", proceso: "Embutido de chumacera", eph: 11.167 },
+  { maquina: "Troqueladora 6", proceso: "Formado en pata en V", eph: 6.667 },
+  { maquina: "Troqueladora 6", proceso: "Tubo", eph: 2.8 },
+  { maquina: "Cab. Soldadura 1", proceso: "Capirucho", eph: 3.333 },
+  { maquina: "Cab. Soldadura 2", proceso: "Dado", eph: 2.667 },
+  { maquina: "Sierra de Cinta", proceso: "Tubito", eph: 0 },
+  { maquina: "Caiman", proceso: "Caiman", eph: 2.917 },
+  { maquina: "Troqueladora de plato", proceso: "Plato", eph: 7.867 },
+  { maquina: "Embutidora 315", proceso: "-", eph: 0 },
+  { maquina: "Lavado de tolva", proceso: "Lavado de tolva", eph: 1.0 },
+  { maquina: "Pintura Naranja", proceso: "-", eph: 0 },
+  { maquina: "Pintura Negra", proceso: "-", eph: 0 },
+  { maquina: "Serigrafía", proceso: "Serigrafía", eph: 3.722 },
+  { maquina: "Ensamble de carreta", proceso: "Ensamble de carreta", eph: 1.0 },
+  { maquina: "Apretado de carreta", proceso: "Apretado de carreta", eph: 1.667 },
+  { maquina: "Troqueladora 7", proceso: "Canal liviana", eph: 7.333 },
+  { maquina: "Troqueladora 7", proceso: "Canal 4.5", eph: 6.2 },
+  { maquina: "Troqueladora 7", proceso: "Formado en pata en V liviana", eph: 14.0 },
+  { maquina: "Troqueladora 7", proceso: "Formado en pata en V 4.5", eph: 14.0 },
 ];
